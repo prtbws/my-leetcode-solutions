a46a3846dd98df8f52bbbf696e9e73f913a5ec64
Partha Biswas <13pb2006@gmail.com>
2026-01-04 06:50:12 +0000

    Sync LeetCode submission - Convert the Temperature (cpp)

---
+++ solutions/problems/convert_the_temperature/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+        double Kelvin = celsius + 273.15, Fahrenheit = celsius * 1.80 + 32.00;
+
+        vector<double>ans;
+
+        ans.push_back(Kelvin);
+        ans.push_back(Fahrenheit);
+
+        return ans;
+    }
+};
