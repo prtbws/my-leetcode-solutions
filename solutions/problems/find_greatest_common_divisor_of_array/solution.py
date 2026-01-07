ea35812ba321a8df8524dae6afd4856941a723ad
Partha Biswas <13pb2006@gmail.com>
2026-01-07 12:58:29 +0000

    Sync LeetCode submission - Find Greatest Common Divisor of Array (python3)

---
+++ solutions/problems/find_greatest_common_divisor_of_array/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findGCD(self, nums: List[int]) -> int:
+        a, b = min(nums), max(nums)
+
+        while b!=0 :
+            a, b = b, a%b
+            
+        return a
