f19812ead394fe82ce10667cd1cee45f8e860cf6
Partha Biswas <13pb2006@gmail.com>
2026-01-15 17:45:20 +0000

    Sync LeetCode submission - Group Anagrams (cpp)

---
+++ solutions/problems/group_anagrams/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        map<map<char,int>, vector<string>> mp;
+
+        for(int i=0; i<strs.size(); i++){
+            map<char,int> count;
+
+            for(char c : strs[i]){
+                count[c]++;
+            }
+
+            mp[count].push_back(strs[i]);
+        }
+
+        vector<vector<string>> ans;
+
+        for(auto &it : mp){
+            ans.push_back(it.second);
+        }
+
+        return ans;
+    }
+};
