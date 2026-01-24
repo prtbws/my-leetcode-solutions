fb5225aea47dd5b6582ca84a4ee2dadbcb392ed4
Partha Biswas <13pb2006@gmail.com>
2026-01-24 06:29:03 +0000

    Sync LeetCode submission - Move Zeroes (cpp)

---
+++ solutions/problems/move_zeroes/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i = 0;
+        int j = 0;
+        while(i < nums.size()){
+            if(nums[i]==0){
+                i++;
+            }
+            else{
+                swap(nums[i], nums[j]);
+                i++;
+                j++;
+            }
+        }
+    }
+};
