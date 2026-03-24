8d9aaeae3f7bde76f2a388d91fe2bd1cecc1aa64
Partha Biswas <13pb2006@gmail.com>
2026-03-24 12:28:40 +0000

    Sync LeetCode submission - Search a 2D Matrix II (cpp)

---
+++ solutions/problems/search_a_2d_matrix_ii/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int r = 0;
+        int c = n-1;
+
+        while(r < m && c >=0){
+
+            if(target > matrix[r][c]) r++;
+            else if(target < matrix[r][c]) c--;
+            else return true;
+        }
+        return false;
+    }
+};
